72b9dd940939c31da314bfc73b6299bd0847d9bf
Bernhard Walser <walserb@student.ethz.ch>
2021-03-29 14:04:24 +0200

    renaming folder fix

--- swin_transformer_pytorch/__init__.py
+++ swin_transformer_pytorch/__init__.py
@@ -1,1 +1,1 @@
-from model.swin_transformer import SwinTransformer, swin_t, swin_s, swin_b, swin_l
+from swin_transformer_pytorch.swin_transformer import SwinTransformer, swin_t, swin_s, swin_b, swin_l
